8109383928cfed68f0c84c90fdf077e4a692dd1b
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2018-04-12 16:25:04 -0700

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.googleCientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accsess, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
